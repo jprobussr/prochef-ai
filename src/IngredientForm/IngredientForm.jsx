9ae0bbbf06c6b25029d1c34fb53f816d05bf3ede
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-07-08 12:03:24 -0500

    Add ingredient form state handling

--- src/IngredientForm/IngredientForm.jsx
+++ src/IngredientForm/IngredientForm.jsx
@@ -1,16 +1,42 @@
 import './IngredientForm.css';
+import { useState } from 'react';
 
 const IngredientForm = () => {
+  const [ingredient, setIngredient] = useState('');
+
+  const handleIngredientChange = (e) => {
+    console.log(e.target.value);
+    setIngredient(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const trimmedIngredient = ingredient.trim();
+
+    if (!trimmedIngredient) {
+        return;
+    }
+
+    console.log(trimmedIngredient);
+    setIngredient('');
+  }
+
   return (
-    <section className='ingredient-form-section'>
-        <h2>What's in your kitchen?</h2>
+    <section className="ingredient-form-section">
+      <h2>What's in your kitchen?</h2>
 
-        <form className="ingredient-form">
-            <input type="text" placeholder='e.g. chicken, rice, broccoli' />
-            <button>Add Ingredient</button>
-        </form>
+      <form className="ingredient-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="e.g. chicken, rice, broccoli"
+          value={ingredient}
+          onChange={handleIngredientChange}
+        />
+        <button>Add Ingredient</button>
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default IngredientForm
+export default IngredientForm;
