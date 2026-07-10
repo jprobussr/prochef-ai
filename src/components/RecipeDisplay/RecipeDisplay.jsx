a1e31d4915c4527bb3b59c0d931407a5f8e3c0bf
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-07-10 16:42:38 -0500

    style: render recipe markdown with dark theme

--- src/components/RecipeDisplay/RecipeDisplay.jsx
+++ src/components/RecipeDisplay/RecipeDisplay.jsx
@@ -1,10 +1,13 @@
 import './RecipeDisplay.css';
+import ReactMarkdown from 'react-markdown';
 
 const RecipeDisplay = ({ recipe }) => {
   return (
     <section className="recipe-display">
       <h2>Your Recipe</h2>
-      <pre>{recipe}</pre>
+      <div className="recipe-display__content">
+        <ReactMarkdown>{recipe}</ReactMarkdown>
+      </div>
     </section>
   );
 };
